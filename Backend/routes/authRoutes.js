4324f2a70592d0df4232dc30f6fe8ecce08b994b
ketanxyz <ketangupta889@gmail.com>
2026-07-06 15:15:32 +0530

    middleware implementation for auth & admin

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, getUsers } = require("../controllers/authController.js");
+const { registerUser, loginUser, getUsers } = require("../controllers/authController");
+const { protect } = require("../middleware/authMiddleware");
+const admin = require("../middleware/adminMiddleware");
 
 
 router.post("/register", registerUser);
