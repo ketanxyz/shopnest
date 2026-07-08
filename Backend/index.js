b102ef98643751a9b8945df8ca0873db2993c01e
ketanxyz <ketangupta889@gmail.com>
2026-07-08 20:30:13 +0530

    added the Product creation routes and logic

--- Backend/index.js
+++ Backend/index.js
@@ -7,12 +7,18 @@ connectDB();
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Shopnest Backend is running Properly");
 });
 
-app.use('api/auth', require('./routes/authRoutes.js'));
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/products', require('./routes/productRoutes'));
+// app.use('/api/orders', require('./routes/orderRoutes'));
+// app.use('/api/payment', require('./routes/paymentRoutes'));
+// app.use('/api/analytics', require('./routes/analyticsRoutes'));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
